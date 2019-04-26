159a0dab3342aafe691edaa35d072dbe45ad5d06
lichen <1010043642@qq.com>
2019-04-26 16:34:15 +0800

    quanbu

---
+++ Demo/SequencerDemo/DelegateCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequencerDemo
+{
+    public class DelegateCollection
+    {
+        public delegate void buttonClick();
+    }
+}
---
+++ Demo/SequencerDemo/Form1.cs
@@ -0,0 +1,401 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Sanford.Multimedia.Midi;
+using Sanford.Multimedia.Midi.UI;
+
+namespace SequencerDemo
+{
+    public partial class Form1 : Form
+    {
+        private bool scrolling = false;
+
+        private bool playing = false;
+
+        private bool closing = false;
+
+        private OutputDevice outDevice;
+
+        private int outDeviceID = 0;
+
+        private OutputDeviceDialog outDialog = new OutputDeviceDialog();
+
+        private bool rdbcheck = false;
+        private bool isReplay = false;
+        //public DelegateCollection.buttonClick myButtonClick;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if(OutputDevice.DeviceCount == 0)
+            {
+                MessageBox.Show("No MIDI output devices available.", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                Close();
+            }
+            else
+            {
+                try
+                {
+                    outDevice = new OutputDevice(outDeviceID);
+
+                    sequence1.LoadProgressChanged += HandleLoadProgressChanged;
+                    sequence1.LoadCompleted += HandleLoadCompleted;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                    Close();
+                }
+            }
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            pianoControl1.PressPianoKey(e.KeyCode);
+
+            base.OnKeyDown(e);
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            pianoControl1.ReleasePianoKey(e.KeyCode);
+
+            base.OnKeyUp(e);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            closing = true;
+
+            base.OnClosing(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            sequence1.Dispose();
+
+            if(outDevice != null)
+            {
+                outDevice.Dispose();
+            }
+
+            outDialog.Dispose();
+
+            base.OnClosed(e);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openMidiFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openMidiFileDialog.FileName;
+                Open(fileName);
+            }
+        }
+
+        public void Open(string fileName)
+        {
+            try
+            {
+                sequencer1.Stop();
+                playing = false;
+                sequence1.LoadAsync(fileName);
+                this.Cursor = Cursors.WaitCursor;
+                startButton.Enabled = false;
+                continueButton.Enabled = false;
+                stopButton.Enabled = false;
+                openToolStripMenuItem.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void outputDeviceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutDialog dlg = new AboutDialog();
+
+            dlg.ShowDialog();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                playing = false;
+                sequencer1.Stop();
+                timer1.Stop();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                playing = true;
+                sequencer1.Start();
+                timer1.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                playing = true;
+                sequencer1.Continue();
+                timer1.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void positionHScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            if(e.Type == ScrollEventType.EndScroll)
+            {
+                sequencer1.Position = e.NewValue;
+
+                scrolling = false;
+            }
+            else
+            {
+                scrolling = true;
+            }
+        }
+
+        private void HandleLoadProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            toolStripProgressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void HandleLoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+            startButton.Enabled = true;
+            continueButton.Enabled = true;
+            stopButton.Enabled = true;
+            openToolStripMenuItem.Enabled = true;
+            toolStripProgressBar1.Value = 0;
+
+            if(e.Error == null)
+            {
+                positionHScrollBar.Value = 0;
+                positionHScrollBar.Maximum = sequence1.GetLength();
+            }
+            else
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+        }
+
+        private void HandleChannelMessagePlayed(object sender, ChannelMessageEventArgs e)
+        {
+            if(closing)
+            {
+                return;
+            }
+
+            outDevice.Send(e.Message);
+            pianoControl1.Send(e.Message);
+        }
+
+        private void HandleChased(object sender, ChasedEventArgs e)
+        {
+            foreach(ChannelMessage message in e.Messages)
+            {
+                outDevice.Send(message);
+            }
+        }
+
+        private void HandleSysExMessagePlayed(object sender, SysExMessageEventArgs e)
+        {
+       //     outDevice.Send(e.Message); Sometimes causes an exception to be thrown because the output device is overloaded.
+        }
+
+        private void HandleStopped(object sender, StoppedEventArgs e)
+        {
+            foreach(ChannelMessage message in e.Messages)
+            {
+                outDevice.Send(message);
+                pianoControl1.Send(message);
+            }
+        }
+
+
+        private void HandlePlayingCompleted(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            //MessageBox.Show(isReplay.ToString());
+            //myButtonClick = new DelegateCollection.buttonClick(startButton.PerformClick);
+            if (isReplay)
+            {
+                startButton.PerformClick();
+            }
+        }
+
+        private void pianoControl1_PianoKeyDown(object sender, PianoKeyEventArgs e)
+        {
+            #region Guard
+
+            if(playing)
+            {
+                return;
+            }
+
+            #endregion
+
+            outDevice.Send(new ChannelMessage(ChannelCommand.NoteOn, 0, e.NoteID, 127));
+        }
+
+        private void pianoControl1_PianoKeyUp(object sender, PianoKeyEventArgs e)
+        {
+            #region Guard
+
+            if(playing)
+            {
+                return;
+            }
+
+            #endregion
+
+            outDevice.Send(new ChannelMessage(ChannelCommand.NoteOff, 0, e.NoteID, 0));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(!scrolling)
+            {
+                positionHScrollBar.Value = Math.Min(sequencer1.Position, positionHScrollBar.Maximum);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Resize += new EventHandler(Form1_Resize);
+            old_x = this.Width;
+            old_y = this.Height;
+            SetTag(this);
+            Form1_Resize(new object(), new EventArgs());
+        }
+
+        private void pianoControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openMidiFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private float old_x;
+        private float old_y;
+
+        private void SetTag(Control controls)
+        {
+            foreach(Control control in controls.Controls)
+            {
+                control.Tag = control.Width + ":" + control.Height + ":" + control.Left + ":" + control.Top + ":" + control.Font.Size;
+                if (control.Controls.Count > 0)
+                {
+                    SetTag(control);
+                }
+            }
+        }
+
+        private void SetControls(float new_x, float new_y, Control controls)
+        {
+            foreach(Control control in controls.Controls)
+            {
+                //用正则表达式将上边的字符串拆开
+                string[] mytag = control.Tag.ToString().Split(new char[] { ':' });
+                //宽度
+                float a = Convert.ToSingle(mytag[0]) * new_x;
+                control.Width = (int)a;
+                //高度
+                a = Convert.ToSingle(mytag[1]) * new_y;
+                control.Height = (int)a;
+                //左边距
+                a = Convert.ToSingle(mytag[2]) * new_x;
+                control.Left = (int)a;
+                //上边距
+                a = Convert.ToSingle(mytag[3]) * new_y;
+                control.Top = (int)a;
+                //字体
+                Single newSize = Convert.ToSingle(mytag[4]) * Math.Min(new_x, new_y);
+                control.Font = new Font(control.Font.Name, newSize, control.Font.Style, control.Font.Unit);
+                if (control.Controls.Count > 0)
+                {
+                    SetControls(new_x, new_y, control);
+                }
+            }
+        }
+
+        void Form1_Resize(object sender, EventArgs args)
+        {
+            float new_x = this.Width / old_x;
+            float new_y = this.Height / old_y;
+            SetControls(new_x, new_y, this);
+            this.Text = this.Width.ToString() + " " + this.Height.ToString();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(radioButton1.Checked.ToString());
+            if (rdbcheck)
+            {
+                radioButton1.Checked = false;
+                rdbcheck = false;
+                isReplay = false;
+            }
+            else
+            {
+                radioButton1.Checked = true;
+                rdbcheck = true;
+                isReplay = true;
+            }
+        }
+    }
+}
